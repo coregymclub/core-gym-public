0a98c1884204bbc2f923e60d5cebace39f14d560
coregymclub <gustav.brydner@gmail.com>
2026-01-22 06:35:02 +0100

    Add /login redirect to /logga-in

---
+++ app/pages/login.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// Redirect /login to /logga-in
+definePageMeta({
+  layout: false,
+  middleware: () => {
+    return navigateTo('/logga-in', { redirectCode: 301 })
+  }
+})
+</script>
+
+<template>
+  <div />
+</template>
